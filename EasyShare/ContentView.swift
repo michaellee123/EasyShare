780058917775808c861ac6bc28fe7e4e701b37d8
michaellee <jjjamdmin@outlook.com>
2020-05-02 23:42:29 +0800

    Initial Commit

---
+++ EasyShare/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  EasyShare
+//
+//  Created by Michael Lee on 2020/5/2.
+//  Copyright © 2020 Michael Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
