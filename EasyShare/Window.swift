59dc6a0f79a51ba75138211f1a494e004217b829
Michael Lee <jjjamdmin@outlook.com>
2020-05-07 11:41:16 +0800

    1、真假launchpad的icon；2、最低支持版本到10.13

---
+++ EasyShare/Window.swift
@@ -0,0 +1,17 @@
+//
+//  Window.swift
+//  EasyShare
+//
+//  Created by Michael Lee on 2020/5/6.
+//  Copyright © 2020 Michael Lee. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+import SwiftUI
+
+class Window :NSWindowController{
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    }
+}
